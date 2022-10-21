5e8818275d79dccd9d0bebcd6a82e6e480dc2f9a
sittan2 <hang5050@naver.com>
2022-10-22 03:44:07 +0900

    Managers 수정

---
+++ Assets/Scripts/Managers.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Managers : MonoBehaviour
+{
+    private static Managers instance;
+    public static Managers Instance { get { return instance; } }
+
+    #region Managers
+    private GameManager _game = new GameManager();
+    public static GameManager Game { get { return Instance._game; } }
+    #endregion
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if (instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            instance = go.GetComponent<Managers>();
+            //Data.Init();
+        }
+    }
+
+    private void Clear()
+    {
+
+    }
+}
